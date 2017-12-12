4e83d2438b07cd944995c08724b5cca2a36535ed
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-12 12:26:46 -0600

    Added forLoop to var getButtonVal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 
 	var jewelValues;
 	var getButtonVal;
+	var redJewel;
+	var blueJewel;
+	var yellowJewel;
+	var greenJewel;
 	var ranNum;
 	var wins;
 	var losses;
@@ -13,6 +17,9 @@ $(document).ready(function() {
 			min = Math.ceil(1);
 			max = Math.floor(12);
 			return Math.floor(Math.random() * (12 - 1)) + 1;
+			for (var i = 0; i < jewelValues.length; i++) {
+				getButtonVal(jewelValues[i]);
+			};
 		};
 		wins = "";
 		losses = "";
@@ -21,16 +28,26 @@ $(document).ready(function() {
 			max = Math.floor(120);
 			return Math.floor(Math.random() * (120 - 19)) + 19;
 		};
+		redJewel = jewelValues[0];
+		blueJewel = jewelValues[1];
+		yellowJewel = jewelValues[2];
+		greenJewel = jewelValues[3];
 
-		
 
-		$.each(jewelValues)
+
+		getButtonVal();
 
 		$("#random-number").text(ranNum);
 
+		/*jewelValues.forEach(getButtonVal("") {
+
+		});*/
+
 		$("#wins #losses #total-score").empty();
 
-		jewelValues
+
+
+		
 
 	};
 
