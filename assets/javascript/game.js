0c02911f917eebc848b7ccc67bd5dff72975a54d
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-12 10:12:49 -0600

    Just another commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	var jewelValues;
+	var getButtonVal;
 	var ranNum;
 	var wins;
 	var losses;
@@ -8,22 +9,33 @@ $(document).ready(function() {
 	function initializeGame() {
 
 		jewelValues = ["", "", "", ""];
+		getButtonVal = function(min, max) {
+			min = Math.ceil(1);
+			max = Math.floor(12);
+			return Math.floor(Math.random() * (12 - 1)) + 1;
+		};
 		wins = "";
 		losses = "";
 		ranNum = function(min, max) {
 			min = Math.ceil(19);
 			max = Math.floor(120);
-			return Math.floor(Math.random() * (120 - 19) + 19);
-			$("#random-number").text(ranNum);
-			console.log(ranNum);
+			return Math.floor(Math.random() * (120 - 19)) + 19;
 		};
 
-		ranNum();
+		
 
-	}
+		$.each(jewelValues)
+
+		$("#random-number").text(ranNum);
+
+		$("#wins #losses #total-score").empty();
+
+		jewelValues
+
+	};
 
 
 	initializeGame();
 	
-
+	console.log(jewelValues);
 });
