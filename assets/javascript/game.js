aa7388ea79542a6a963245c026b193fe929f4bb2
JonathonMoore1 <30606521+JonathonMoore1@users.noreply.github.com>
2017-12-14 18:22:40 -0600

    Final changes before submission -- no reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,106 @@
-$(document).ready(function() {
+// =============
+// App variables
+// =============
 
-	var jewelValues;
-	var getButtonVal;
-	var redJewel;
-	var blueJewel;
-	var yellowJewel;
-	var greenJewel;
-	var ranNum;
-	var wins;
-	var losses;
-	var totalScore;
+var jewelValues;
+var redJewel;
+var blueJewel;
+var yellowJewel;
+var greenJewel;
+var targetNum;
+var wins;
+var losses;
+var totalScore;
 
-	function initializeGame() {
+// ==========
+// Functions
+// ==========
 
-		jewelValues = ["", "", "", ""];
-		getButtonVal = function() {
-			var min = 1;
-			var max = 12;
-			return Math.floor(Math.random() * (12 - 1)) + 1;
-		};
-		for (var i = 0; i < jewelValues.length; i++) {
-				jewelValues[i] = getButtonVal();	
-			};
-		wins = 0;
-		losses = 0;
-		ranNum = function(min, max) {
-			min = Math.ceil(19);
-			max = Math.floor(120);
-			return Math.floor(Math.random() * (120 - 19)) + 19;
-		};
-		totalScore = 0;
-		redJewel = jewelValues[0];
-		blueJewel = jewelValues[1];
-		yellowJewel = jewelValues[2];
-		greenJewel = jewelValues[3];
+function ranNum(min, max) {
+	return Math.floor(Math.random() * (max - min)) + min;
+};
 
+ranNum(1, 12);
 
-		$("#random-number").text(ranNum);
-
-		$("#wins #losses #total-score").empty();
-
-
-		$("#red-jewel").on("click", function() {
-			totalScore += redJewel;
-			$("#total-score").text(totalScore);
-		});
-
-		$("#blue-jewel").on("click", function() {
-			totalScore += blueJewel;
-			$("#total-score").text(totalScore);
-		});
-
-		$("#yellow-jewel").on("click", function() {
-			totalScore += yellowJewel;
-			$("#total-score").text(totalScore);
-		});
-
-		$("#green-jewel").on("click", function() {
-			totalScore += greenJewel;
-			$("#total-score").text(totalScore);
-		});
-
-		ranNum = parseInt(ranNum);
-
-		if (totalScore === ranNum) {
-			wins++;
-			$("#wins").append("<strong>" + wins + "</strong>");
-			console.log("You won!");
-		};
-		
-
+function hasWon() {
+	if (totalScore === targetNum) {
+		$("total-score").text(totalScore);
+		wins++;
+		$("#message").text("You won!!")
+		$("#wins").append(wins);
+		console.log("You won!");
 	};
+};
 
+function hasLost() {
+	if (totalScore > targetNum) {
+		$("total-score").text(totalScore);
+		losses++;
+		$("#message").text("You lost!!")
+		$("#losses").append(losses);
+		console.log("You lost!");
+	}
+}
+function initializeGame() {
+
+	jewelValues = ["", "", "", ""];
+
+	/*I tried to write the following line in such a way that there if there were duplicates the function would be called coninually until all values were different. I gave up.*/
+	for (var i = 0; i < jewelValues.length; i++) {
+			jewelValues[i] = ranNum(1, 12);	
+		};
+	wins = 0;
+	losses = 0;
+	totalScore = 0;
+	winner = false;
+	loser = false;
+	redJewel = jewelValues[0];
+	blueJewel = jewelValues[1];
+	yellowJewel = jewelValues[2];
+	greenJewel = jewelValues[3];
+	targetNum = ranNum(19, 120);
+
+	$("#random-number").text(targetNum);
+
+	$("#wins #losses #total-score").empty();
+
+	//===============
+	//On-Click Events
+	//===============
+
+	$("#red-jewel").on("click", function() {
+		totalScore += redJewel;
+		$("#total-score").text(totalScore);
+		hasWon();
+		hasLost();
+	});
+
+	$("#blue-jewel").on("click", function() {
+		totalScore += blueJewel;
+		$("#total-score").text(totalScore);
+		hasWon();
+		hasLost();
+	});
+
+	$("#yellow-jewel").on("click", function() {
+		totalScore += yellowJewel;
+		$("#total-score").text(totalScore);
+		hasWon();
+		hasLost();
+	});
+
+	$("#green-jewel").on("click", function() {
+		totalScore += greenJewel;
+		$("#total-score").text(totalScore);
+		hasWon();
+		hasLost();
+	});
+};
+
+$(document).ready(function() {
 
 	initializeGame();
 	
 	console.log(jewelValues);
-	console.log(ranNum);
+	console.log(targetNum);
 });
