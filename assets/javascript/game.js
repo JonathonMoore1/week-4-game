8dbd6cabb842bff6c20564c3254c38c8ec5c6dec
Jonathon Moore <jonmoore510@gmail.com>
2018-05-08 15:19:17 -0500

    Fixed counter issues and cleaned up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,106 +1,96 @@
-// =============
-// App variables
-// =============
+// Starting game state
+// =====================
+var jewels = {
+	red: {name: "Red", value: 0},
+	blue: {name: "Blue", value: 0},
+	yellow: {name: "Yellow", value: 0},
+	green: {name: "Green", value: 0}
+};
 
-var jewelValues;
-var redJewel;
-var blueJewel;
-var yellowJewel;
-var greenJewel;
-var targetNum;
-var wins;
-var losses;
-var totalScore;
+var currentScore = 0;
+var targetScore = 0;
 
-// ==========
-// Functions
-// ==========
+var wins = 0;
+var losses = 0;
 
+//==============================
 function ranNum(min, max) {
 	return Math.floor(Math.random() * (max - min)) + min;
 };
 
-ranNum(1, 12);
+function initializeGame() {
+
+	currentScore = 0;
+
+	targetScore = ranNum(19, 120);
+
+	jewels.red.value = ranNum(1, 12);
+	jewels.blue.value = ranNum(1, 12);
+	jewels.yellow.value = ranNum(1, 12);
+	jewels.green.value = ranNum(1, 12);
+
+	$("#target-score").html(targetScore);
+	$("#current-score").html(currentScore);
+	$("#wins").html("Wins: " + wins);
+	$("#losses").html("Losses: " + losses);
+
+	console.log("-----------------------------------");
+  console.log("Target Score: " + targetScore);
+  console.log("Red: " + jewels.red.value + " | Blue: " + jewels.blue.value + " | Yellow: " + jewels.yellow.value +
+    " | Green: " + jewels.green.value);
+  console.log("-----------------------------------");
+
+};
 
 function hasWon() {
-	if (totalScore === targetNum) {
-		$("total-score").text(totalScore);
+	if (currentScore > targetScore) {
+		$("#current-score").html(currentScore);
+		losses++;
+		alert("You lost!!");
+		$("#losses").text(losses);
+		console.log("You lost!");
+		initializeGame();
+	}
+	else if (currentScore === targetScore) {
+		$("#current-score").html(currentScore);
 		wins++;
-		$("#message").text("You won!!")
-		$("#wins").append(wins);
-		console.log("You won!");
+		alert("You've won!!");
+		$("#wins").text(wins);
+		console.log("You've won!");
+		initializeGame();
 	};
 };
 
-function hasLost() {
-	if (totalScore > targetNum) {
-		$("total-score").text(totalScore);
-		losses++;
-		$("#message").text("You lost!!")
-		$("#losses").append(losses);
-		console.log("You lost!");
-	}
+function addValues(clickedJewel) {
+	currentScore += clickedJewel.value;
+	$("#current-score").html(currentScore);
+	hasWon();
+	console.log("Your score: " + currentScore);
 }
-function initializeGame() {
 
-	jewelValues = ["", "", "", ""];
-
-	/*I tried to write the following line in such a way that there if there were duplicates the function would be called coninually until all values were different. I gave up.*/
-	for (var i = 0; i < jewelValues.length; i++) {
-			jewelValues[i] = ranNum(1, 12);	
-		};
-	wins = 0;
-	losses = 0;
-	totalScore = 0;
-	winner = false;
-	loser = false;
-	redJewel = jewelValues[0];
-	blueJewel = jewelValues[1];
-	yellowJewel = jewelValues[2];
-	greenJewel = jewelValues[3];
-	targetNum = ranNum(19, 120);
-
-	$("#random-number").text(targetNum);
-
-	$("#wins #losses #total-score").empty();
-
-	//===============
-	//On-Click Events
-	//===============
-
-	$("#red-jewel").on("click", function() {
-		totalScore += redJewel;
-		$("#total-score").text(totalScore);
-		hasWon();
-		hasLost();
-	});
-
-	$("#blue-jewel").on("click", function() {
-		totalScore += blueJewel;
-		$("#total-score").text(totalScore);
-		hasWon();
-		hasLost();
-	});
-
-	$("#yellow-jewel").on("click", function() {
-		totalScore += yellowJewel;
-		$("#total-score").text(totalScore);
-		hasWon();
-		hasLost();
-	});
-
-	$("#green-jewel").on("click", function() {
-		totalScore += greenJewel;
-		$("#total-score").text(totalScore);
-		hasWon();
-		hasLost();
-	});
-};
-
-$(document).ready(function() {
+$(document).ready(function () {
 
 	initializeGame();
+	$("#red-jewel").on("click", function() {
+		addValues(jewels.red);
+		console.log("clicked");
+	});
 	
-	console.log(jewelValues);
-	console.log(targetNum);
+	$("#blue-jewel").on("click", function() {
+		addValues(jewels.blue);
+		console.log("clicked")
+	});
+	
+	$("#yellow-jewel").on("click", function() {
+		addValues(jewels.yellow);
+		console.log("clicked")
+	});
+	
+	$("#green-jewel").on("click", function() {
+		addValues(jewels.green);
+		console.log("clicked")
+	});
+
+	console.log(jewels);
+	console.log(targetScore);
 });
